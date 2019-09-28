8c97a30c06f73563d0d23edefe81711633d1ff50
CodingCloud <CodingCloud9527@gmail.com>
2019-09-28 17:28:11 +0800

    adjust structure

---
+++ src/BPMN.Analyze/Model/FlowComponent.cs
@@ -0,0 +1,9 @@
+﻿namespace BPMN.Analyze.Model
+{
+    internal abstract class FlowComponent
+    {
+        public string Id { get; protected set; }
+
+        public string Name { get; protected set; }
+    }
+}
