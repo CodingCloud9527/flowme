7c762d7005ea0a4463df1eccb1ba1ced756367de
CodingCloud9527 <CodingCloud9527@gmail.com>
2019-12-15 21:46:48 +0800

    <no comment>

--- src/FlowMe.Analyze/Diagram/IDiagram.cs
+++ src/FlowMe.Analyze/Diagram/IDiagram.cs
@@ -3,9 +3,6 @@ using FlowMe.Analyze.Model.Event;
 
 namespace FlowMe.Analyze.Diagram
 {
-    /// <summary>
-    ///     Describe the relation of components.
-    /// </summary>
     public interface IDiagram
     {
         StartEvent GetEntry();
